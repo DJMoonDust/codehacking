5293fc72e6438515c3c5755e8f8f919b710bffb0
Aaron Moon <talents4Him@gmail.com>
2019-04-10 15:38:36 -0400

    able to create post with static category

--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -4,4 +4,35 @@
 
     <h1>Posts</h1>
 
+    <table class="table">
+        <thead>
+            <tr>
+                <th>ID</th>
+                <th>User</th>
+                <th>Category</th>
+                <th>Photo</th>
+                <th>Title</th>
+                <th>Body</th>
+                <th>Created At</th>
+                <th>Updated At</th>
+            </tr>
+        </thead>
+        <tbody>
+            @if($posts)
+                @foreach($posts as $post)
+                    <tr>
+                        <td>{{ $post->id }}</td>
+                        <td>{{ $post->user->name }}</td>
+                        <td>{{ $post->category_id }}</td>
+                        <td><img height="50px" src="{{ $post->photo ? $post->photo->file : '/images/placeholder3.jpg' }}" ></td>
+                        <td>{{ $post->title }}</td>
+                        <td>{{ $post->body }}</td>
+                        <td>{{ $post->created_at->diffForhumans() }}</td>
+                        <td>{{ $post->updated_at->diffForhumans() }}</td>
+                    </tr>
+                @endforeach
+            @endif
+        </tbody>
+    </table>
+
 @stop
