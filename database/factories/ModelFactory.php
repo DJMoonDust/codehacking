792e392adc5a5b2c125a1f2fc0ce92c544670887
Aaron Moon <talents4Him@gmail.com>
2019-11-25 10:26:46 -0500

    added seeders and prior to updating to laravel 5.5

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -13,9 +13,74 @@
 
 $factory->define(App\User::class, function (Faker\Generator $faker) {
     return [
+        'role_id'=>$faker->numberBetween(1,3),
+        'is_active'=>$faker->numberBetween(0,1),
         'name' => $faker->name,
         'email' => $faker->safeEmail,
         'password' => bcrypt(str_random(10)),
         'remember_token' => str_random(10),
+//        'photo_id'=>$faker->numberBetween(0,8),
     ];
 });
+
+$factory->define(App\Post::class, function (Faker\Generator $faker) {
+    return [
+//        'user_id'=>$faker->numberBetween(1,13),
+        'category_id'=>$faker->numberBetween(1,7),
+//        'photo_id'=>$faker->numberBetween(0,8),
+        'title'=> $faker->sentence(7,11),
+        'body'=> $faker->paragraphs(rand(10,15), true),
+        'slug' => $faker->slug(),
+    ];
+});
+
+$factory->define(App\Role::class, function (Faker\Generator $faker) {
+    return [
+
+        'name'=>$faker->randomElement(['administrator', 'author', 'subscriber']),
+
+    ];
+});
+
+$factory->define(App\Category::class, function (Faker\Generator $faker) {
+    return [
+
+        'name'=>$faker->randomElement(['Laravel', 'PHP', 'Programming', 'JavaScript', 'Bootstrap', 'JAVA', 'HTML', 'Misc.' ]),
+
+    ];
+});
+
+$factory->define(App\Photo::class, function (Faker\Generator $faker) {
+    return [
+
+        'file'=>'placeholder.jpg',
+
+    ];
+});
+
+$factory->define(App\Comment::class, function (Faker\Generator $faker) {
+    return [
+
+        'Post_id'=>$faker->numberBetween(1,10),
+        'is_active'=>$faker->numberBetween(0,1),
+        'author'=>$faker->name,
+        'photo'=>'placeholder.jpg',
+        'email'=>$faker->safeEmail,
+        'body'=>$faker->paragraphs(1, true),
+
+    ];
+});
+
+$factory->define(App\CommentReply::class, function (Faker\Generator $faker) {
+    return [
+
+        'comment_id'=>$faker->numberBetween(1,10),
+        'is_active'=>$faker->numberBetween(0,1),
+        'author'=>$faker->name,
+        'photo'=>'placeholder.jpg',
+        'email'=>$faker->safeEmail,
+        'body'=>$faker->paragraphs(1, true),
+
+    ];
+});
+
---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+
+        DB::table('users')->insert([
+
+
+            'role_id'=>2,
+            'is_active'=>1,
+            'name'=>str_random(10),
+            'email'=>str_random(10).'@email.com',
+            'password'=>bcrypt('secret')
+
+        ]);
+
+    }
+}
